140d5f44287031ac287423f9d87fa3ab4f480bdf
gopika <gopikaksurendran@gmail.com>
2024-08-21 10:22:52 +0530

    Added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
-document.getElementById("round").addEventListener('mousemove',(event)=> {
+document.getElementById("rounds").addEventListener('mousemove',(event)=> {
     const x = event.clientX; // Mouse position relative to the viewport
     const y = event.clientY; // Mouse position relative to the viewport
-    document.getElementById("round").style.top=x+"px"
-    document.getElementById("round").style.left=y+"px"
-},2000)
+    document.getElementById("round").style.left=x+"px"
+    document.getElementById("round").style.top=y+"px"
+})
